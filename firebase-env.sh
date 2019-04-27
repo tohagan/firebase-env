fd960235519f222b1e15adcd68128acf07ace8b6
Tony OHagan <tony@ohagan.name>
2019-04-27 15:20:20 +1000

    firebase-env.sh: Switch firebase projects

---
+++ firebase-env.sh
@@ -0,0 +1,31 @@
+## Switch Firebase project (environment)
+# Usage: $ . ./firebase-env.sh [test|dev|stage|prod]
+
+# Info:
+# - https://firebase.google.com/docs/functions/config-env
+# - https://medium.com/google-cloud/firebase-separating-configuration-from-code-in-admin-sdk-d2bcd2e87de6
+
+[ -z "$1" ] || firebase use $1
+
+export FIREBASE_TOKEN=`cat keys/token`
+export GCLOUD_PROJECT=`firebase use`
+export GOOGLE_APPLICATION_CREDENTIALS=`realpath keys/$GCLOUD_PROJECT.json`
+export FIREBASE_CONFIG=`realpath ./config.json`
+
+## config.json (add to .gitignore)
+cat >./config.json <<!
+{
+  "databaseURL": "https://${GCLOUD_PROJECT}.firebaseio.com",
+  "projectId": "${GCLOUD_PROJECT}",
+  "storageBucket": "${GCLOUD_PROJECT}.appspot.com"
+}
+!
+
+cat <<!
+GCLOUD_PROJECT=$GCLOUD_PROJECT
+GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS
+
+FIREBASE_TOKEN set
+FIREBASE_CONFIG=$FIREBASE_CONFIG
+!
+cat config.json
